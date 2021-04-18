16fc34be4c482f495b0235d19836b77de9f39f35
ivand1988 <ivand1988@outlook.com>
2021-04-18 18:48:56 +0300

    change sql schema

--- db.py
+++ db.py
@@ -1,40 +1,71 @@
 import sqlite3
+from datetime import datetime, timedelta
+
+
+now = datetime.now()
+lastmonth = now - timedelta(weeks=6)
+endoflastmonth = lastmonth.replace(day=30)
+month2 = endoflastmonth.strftime("%Y-%m-%d")
+
+this_month_raw = now.replace(day=1)
+this_month = this_month_raw.strftime("%Y-%m-%d")
+
 
 conn = sqlite3.connect("db.db")
 cur = conn.cursor()
 
+def get_user_id(name):
+    cur.execute("SELECT id FROM users WHERE name = (?)", (name,))
+    result = cur.fetchone()[0]
+    return result
+
+def get_category_id(name):
+    cur.execute("SELECT id FROM category WHERE name = (?)", (name,))
+    result = cur.fetchone()[0]
+    return result
+
 def user_check(user, id):
-    cur.execute("SELECT telegram_id FROM Users WHERE name = (?)", (user,))
+    cur.execute("SELECT telegram_id FROM users WHERE name = (?)", (user,))
     result = cur.fetchone()
     if result:
         return f"Hello {user}!"
     else:
-        cur.execute("INSERT INTO Users (telegram_id, name) VALUES (?, ?)", (id, user,))
+        cur.execute("INSERT INTO users (telegram_id, name) VALUES (?, ?)", (id, user,))
         conn.commit()
         return f"Welcome new user {user}, lets start!"
 
 def select_categories():
-    cur.execute("PRAGMA table_info(spends)")
-    result = cur.fetchall()[2:]
+    cur.execute("SELECT * FROM category")
+    result = cur.fetchall()
     lst = list()
     for item in result:
         lst.append(item[1])
-    lst.sort()
+    #lst.sort()
     return lst
 
 def add_spend(user,category,amount):
-    categories = select_categories()
-    if category in categories:
-        cur.execute(f"""INSERT INTO spends (user_id, date, {category})
-                        VALUES (?, datetime('now','localtime'), ?)""",
-                    (user, amount,))
+    username_id = get_user_id(user)
+    category_id = get_category_id(category)
+    try:
+        cur.execute("""INSERT INTO spend (user_id, category_id, amount, date)
+                     VALUES (?, ?, ?, datetime('now','localtime'))""",
+                     (username_id, category_id, amount,))
         conn.commit()
         return "Done!"
-    else:
+    except:
         return "Wrong category!"
 
-def get_today_statistics(user, category):
-        cur.execute(f"""SELECT SUM(spends.{category}) FROM spends WHERE user_id = (?)
-                        AND date(date) = date('now')""", (user,))
-        result = cur.fetchall()[0][0]
-        return result
+def get_today_statistics(user,category):
+    username_id = get_user_id(user)
+    category_id = get_category_id(category)
+    cur.execute(f"""SELECT SUM(amount) FROM spend WHERE user_id = (?) AND category_id = (?)
+                        AND date(date) = date('now')""", (username_id, category_id))
+    result = cur.fetchone()[0]
+    return result
+
+
+def get_month_statistics(user,category):
+    cur.execute(f"""SELECT SUM(spends.{category}) FROM spends WHERE user_id = (?)
+                    AND date(date) >= date({this_month})""", (user,))
+    result = cur.fetchall()[0][0]
+    return result
--- start.py
+++ start.py
@@ -15,11 +15,6 @@ from aiogram.utils import executor
 from datetime import datetime, timedelta
 import db
 
-"""
-now = datetime.now()
-lastmonth = now - timedelta(weeks=6)
-endoflastmonth = lastmonth.replace(day=30)
-month2 = endoflastmonth.strftime("%Y-%m-%d")"""
 
 bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
@@ -45,9 +40,11 @@ async def cmd_start(message: types.Message):
     chat_id = message.chat.id
     username = message.chat.username
     check_user = db.user_check(username, chat_id)
-    await message.answer(f"<b>{check_user}</b>", reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
+    await message.answer(f"<b>{check_user}</b>", reply_markup=keyboard,
+                           parse_mode=types.ParseMode.HTML)
 
 
+"""Add Spends menu"""
 @dp.message_handler(Text(equals="Add spends"))
 async def cmd_add_spends(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -58,15 +55,18 @@ async def cmd_add_spends(message: types.Message):
         keyboard.add(item)
     spends_ = "\n".join(str(el) for el in spends)
     spends_clear = spends_.replace('(','').replace(')','').replace("'","")
-    await message.answer(f"Please, choose one category.\nOr choose 'Type'", reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
+    await message.answer(f"{spends}Please, choose one category.\nOr choose 'Type'", reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
 
+
+"""Selected category"""
 user_data = {}
 @dp.message_handler(text=db.select_categories())
 async def text_in_handler(message: types.Message):
     user_data[message.chat.username] = message.text
     category = message.text
     await Form2.spends_write.set()
-    await message.answer(f"Please write amount for category:\n<b>{category}</b>", parse_mode=types.ParseMode.HTML)
+    await message.answer(f"Please write amount for category:\n<b>{category}</b>",
+                         parse_mode=types.ParseMode.HTML)
 
 @dp.message_handler(state=Form2.spends_write)
 async def cmd_write_spends(message: types.Message, state: FSMContext):
@@ -77,13 +77,16 @@ async def cmd_write_spends(message: types.Message, state: FSMContext):
     await state.finish()
     await message.answer(f"<u>Categoty: {category}\nSpends: {spends}</u>\n<b>{write}</b>", parse_mode=types.ParseMode.HTML)
 
+
+"""Type spends"""
 @dp.message_handler(Text(equals="Type"))
 async def cmd_type_spends(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Cancel", "Back"]
     keyboard.add(*buttons)
     await Form.spends_type.set()
-    await message.answer(f"Please type a spends\n<i>Examples:\nbar 800\nfood 1000</i>", reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
+    await message.answer(f"Please type a spends\n<i>Examples:\nbar 800\nfood 1000</i>",
+                         reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
 
 @dp.message_handler(Text(equals="Cancel"), state="*", )
 async def cancel_handler(message: types.Message, state: FSMContext):
@@ -106,13 +109,24 @@ async def cmd_type_spends(message: types.Message, state: FSMContext):
         amount = float(message_split[1])
         write = db.add_spend(username,category,amount)
         await state.finish()
-        await message.answer(f"<u>Categoty: {message_split[0]}\nSpends: {message_split[1]}</u>\n<b>{write}</b>", parse_mode=types.ParseMode.HTML)
+        await message.answer(f"<u>Categoty: {message_split[0]}\n"
+                             f"Spends: {message_split[1]}</u>\n<b>{write}</b>", 
+                              parse_mode=types.ParseMode.HTML)
     else:
         await message.answer(f"{username} Please use existing categories!")
 
 
+"""Statistics"""
 @dp.message_handler(Text(equals="Statistics"))
 async def cmd_statistics(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Daily", "Month", "Back", "Delete"]
+    keyboard.add(*buttons)
+    await message.answer("Statistics", reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Daily"))
+async def cmd_daily_statistics(message: types.Message):
     stat = list()
     total = 0
     username = message.chat.username
@@ -126,39 +140,32 @@ async def cmd_statistics(message: types.Message):
             stat.append(newtup)
     spends_ = "\n".join(str(el) for el in stat)
     spends_clear = spends_.replace('(','').replace(')','').replace("'","")
-    await message.answer(f"{spends_clear}\nTotal\n{total}")
+    await message.answer(f"<u>{spends_clear}</u>\n<b>Total\n{total}</b>", parse_mode=types.ParseMode.HTML)
 
-@dp.message_handler(Text(equals="Config spends"))
+@dp.message_handler(Text(equals="Month"))
 async def cmd_spends(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Back"]
     keyboard.add(*buttons)
-    await message.answer("How to use:\n", reply_markup=keyboard)
+    username = message.chat.username
+    categories = db.select_categories()
+    stat = list()
+    total = 0
+    for category in categories:
+        month_stat = db.get_month_statistics(username,category)
+        if month_stat is None: continue
+        else:
+            total = total + month_stat
+            newtup = (category, month_stat)
+            stat.append(newtup)
+    spends_ = "\n".join(str(el) for el in stat)
+    spends_clear = spends_.replace('(','').replace(')','').replace("'","")
+    await message.answer(f"<u>{spends_clear}</u>\n<b>Total\n{total}</b>", parse_mode=types.ParseMode.HTML)
 
-@dp.message_handler(Text(equals="Delete category"))
+@dp.message_handler(Text(equals="Delete spends"))
 async def cmd_delete(message: types.Message):
-    await mesage.answer("here list of categories from db\nChoose one to delete and type")
+    await message.answer("here list of categories from db\nChoose one to delete and type")
 
 if __name__ == "__main__" :
     # Запуск бота
     executor.start_polling(dp, skip_updates=True)
-
-
-
-"""
-spends_raw = input("Enter spends:")
-spends = spends_raw.split()
-category = spends[0]
-amount = float(spends[1])
-print(category, amount)
-"""
-
-"""
-categories = ["food", "bar", "taxi"]
-buttons = ["Type", "Back"]
-lst = categories + buttons"""
-"""
-buttons = ["Type", "Back"]
-lst = spends + buttons
-for item in lst:
-    keyboard.add(item)"""
